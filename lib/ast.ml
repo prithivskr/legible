3ad09ea75cadc9bd758c918060535c8cdab76046
prithivskr <prithivskr@gmail.com>
2026-02-15 23:47:14 -0600

    Fixed precedence issue

--- lib/ast.ml
+++ lib/ast.ml
@@ -3,10 +3,10 @@ type fragment = Text of string | Ref of string * int
 type chunk_def =
   { name: string
   ; is_root: bool
-  ; options: string * string list
+  ; options: (string * string) list
   ; body: fragment list }
 
-type annotation = {name: string; args: string * string list}
+type annotation = {name: string; args: (string * string) list}
 
 type top_level =
   | Prose of fragment list
